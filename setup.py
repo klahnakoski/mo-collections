6eccff6eb4aa83e6d0fabdc7b7074d2e79a78894
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-06-03 19:57:06 -0400

    update version number

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_packages(where='.', lib_prefix='', exclude=()):
 
 setup(
     name='mo-collections',
-    version="1.2.18029",
+    version="2.13.18154",
     description='More Collections! Some useful data structures for dealing with Data',
     long_description=long_desc,
     author='Kyle Lahnakoski',
@@ -52,7 +52,7 @@ setup(
     url='https://github.com/klahnakoski/mo-collections',
     license='MPL 2.0',
     packages=find_packages(".", lib_prefix=""),
-    install_requires=["mo-dots","mo-future","mo-kwargs","mo-logs"],
+    install_requires=["mo-dots>=2.7.18148","mo-future>=2.3.18147","mo-kwargs>=2.13.18154","mo-logs>=2.11.18154"],
     include_package_data=True,
     zip_safe=False,
     classifiers=[  #https://pypi.python.org/pypi?%3Aaction=list_classifiers
