bf080a8befbf9e723ef08bb268ab578b711214ec
Kyle Lahnakoski <kyle@lahnakoski.com>
2019-11-12 17:54:50 -0500

    updates from other projects

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(
     long_description_content_type=str(u'text/markdown'),
     include_package_data=True,
     classifiers=["Development Status :: 4 - Beta","Topic :: Software Development :: Libraries","Topic :: Software Development :: Libraries :: Python Modules","License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)"],
-    install_requires=["mo-dots==2.31.19025","mo-future==2.24.19023","mo-kwargs==2.31.19025","mo-logs==2.31.19025"],
-    version=str(u'2.31.19025'),
+    install_requires=["mo-dots>=3.2.19316","mo-future>=3.1.19316","mo-kwargs>=3.5.19316","mo-logs>=3.5.19316"],
+    version=str(u'3.5.19316'),
     url=str(u'https://github.com/klahnakoski/mo-collections'),
     packages=["mo_collections"],
-    long_description=str(u"More Collections\r\n================\r\n\r\nSome useful data structures for collections of data\r\n\r\n\r\n### Class `Index`\r\n\r\nProvide indexing for a list. Inner properties can be used for keys, and keys can be tuples of properties.  \r\n\r\n### Class `UniqueIndex`\r\n\r\nSame as Index, but includes checks and optimization to ensure members' keys are unique.\r\n\r\n### Class `Queue`\r\n\r\nA `Queue` is a list, with `add()` and `pop()`. It ensures members in the queue are not duplicated by not adding the ones already found in the queue.\r\n\r\n### Class `Matrix`\r\n\r\nA multidimensional grid of values that can be used like a `Mapping` from a-tuple-of-coordinates to the value at that coordinate. Plus a few other convenience methods.\r\n\r\nThis is a naive implementation. The hope it is a simple facade to a faster implementation.\r\n\r\n### Class `Relation`\r\n\r\nStore the many-to-many relations between two domains     "),
+    long_description=str(u"More Collections\n================\n\nSome useful data structures for collections of data\n\n\n### Class `Index`\n\nProvide indexing for a list. Inner properties can be used for keys, and keys can be tuples of properties.  \n\n### Class `UniqueIndex`\n\nSame as Index, but includes checks and optimization to ensure members' keys are unique.\n\n### Class `Queue`\n\nA `Queue` is a list, with `add()` and `pop()`. It ensures members in the queue are not duplicated by not adding the ones already found in the queue.\n\n### Class `Matrix`\n\nA multidimensional grid of values that can be used like a `Mapping` from a-tuple-of-coordinates to the value at that coordinate. Plus a few other convenience methods.\n\nThis is a naive implementation. The hope it is a simple facade to a faster implementation.\n\n### Class `Relation`\n\nStore the many-to-many relations between two domains     "),
     name=str(u'mo-collections')
 )
